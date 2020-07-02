372653cbd1f7fcbfb803b4c7998569785d9db6ae
jimmy <jimmyjaytscheina@gmail.com>
2020-07-02 11:19:33 +0200

    loop through array

---
+++ src/A6.java
@@ -0,0 +1,9 @@
+public class A6 {
+
+    public static void main(String[] args) {
+        int [] myArray = {1, 2,12,15,5,6,8,9,0,10};
+        for (int i = 0; i<myArray.length; i++ ){
+            System.out.println(myArray[i] +" ");
+        }
+    }
+}
